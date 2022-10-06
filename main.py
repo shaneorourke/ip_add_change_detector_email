e17c3c6828dcf81ca0f03a17ce9c9dde7ae56e38
shaneorourke <shaneorourke110@gmail.com>
2022-10-06 13:01:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def convert_bytes(byt):
     output = str(byt, 'UTF-8')
     return output
 
-def write_csv(row):
-    f = open('store_adr', 'w')
+def write_csv(filename,row):
+    f = open(f'{filename}', 'w')
     writer = csv.writer(f)
     writer.writerow([row])
     f.close()
 
-def read_csv_csv():
-    with open('store_adr', mode ='r')as file:
+def read_csv_csv(filename):
+    with open(f'{filename}', mode ='r')as file:
         csvFile = csv.reader(file)
         for lines in csvFile:
             if lines != []:
@@ -28,7 +28,7 @@ if __name__ == '__main__':
 
     if os.path.exists(csv_file_name):
         # Get last recorded IP
-        last_ip = read_csv_csv()[0]
+        last_ip = read_csv_csv(csv_file_name)[0]
 
         # If change detected
         if current_ip != last_ip:
@@ -37,7 +37,7 @@ if __name__ == '__main__':
             Detail:{last_ip} -> {current_ip}"""
 
             # Write new ip to csv file store
-            write_csv(current_ip)
+            write_csv(csv_file_name,current_ip)
             
             # read in json config for smtp details
             with open('config.json') as f:
@@ -48,4 +48,4 @@ if __name__ == '__main__':
             send_email(gmail_user,gmail_pwd,msg_from,to,message)
     else:
         # If no file, then write a new one
-        write_csv(current_ip)
+        write_csv(csv_file_name,current_ip)
